6074979ffa613bc62ddd521b4ad47276beb64319
Jessica McDonald <jessicajsmcdonald@gmail.com>
2024-12-14 16:35:31 +0000

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ActivityMap from './activity-map';
 import Playlist, { SpotifyTrack } from './playlist';
 import { samplePlaylistData } from './samplePlaylisData';
